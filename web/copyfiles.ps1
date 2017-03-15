da4a49db8df951c73183e0b667dc5f170efa675c
Rick Powell <richard.powell@sfwltd.co.uk>
2017-03-15 17:36:07 +0000

    Ensure that files to not get over-written on build

--- web/copyfiles.ps1
+++ web/copyfiles.ps1
@@ -7,15 +7,14 @@ $reactDomSourceDir = "$($PSScriptRoot)\node_modules\react-dom\dist"
 $reactDomTargetDir = "$($distDir)\lib\react-dom\dist"
 $htmlFileSource = "$($PSScriptRoot)\src\index.html"
 
-If (Test-Path $distDir)
+If (-not (Test-Path $distDir))
 {
-    Remove-Item $distDir -Recurse
+    New-Item $distDir -type directory
+    New-Item $reactTargetDir -type directory
+    New-Item $reactDomTargetDir -type directory
 }
 
-New-Item $distDir -type directory
-New-Item $reactTargetDir -type directory
-New-Item $reactDomTargetDir -type directory
-Copy-Item $assetsSourceDir $assetsTargetDir -Recurse
-Copy-Item "$($reactSourceDir)\react.js" $reactTargetDir
-Copy-Item "$($reactDomSourceDir)\react-dom.js" $reactDomTargetDir
-Copy-Item "$($htmlFileSource)" $distDir
+Copy-Item $assetsSourceDir $assetsTargetDir -Recurse -Force
+Copy-Item "$($reactSourceDir)\react.js" $reactTargetDir -Force
+Copy-Item "$($reactDomSourceDir)\react-dom.js" $reactDomTargetDir -Force
+Copy-Item "$($htmlFileSource)" $distDir -Force
