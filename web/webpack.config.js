c6d2a7832fc85b73dca3fd66f16bdc41db492171
Rick Powell <richard.powell@sfwltd.co.uk>
2017-03-15 09:04:03 +0000

    init web project

---
+++ web/webpack.config.js
@@ -0,0 +1,47 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist",
+        publicPath: "http://localhost:8080/"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+        ]        
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 8000,
+            proxy: 'http://localhost:8080/'
+        },
+        {
+            reload: true
+        })
+    ],
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+};
