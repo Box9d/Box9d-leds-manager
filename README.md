1483439b3b582ee89f183978aeb7c08fcb51252c
mwg <matt.george.89@gmail.com>
2017-03-21 11:18:09 +0000

    Add full semantic-ui package to handle custom themes, local fonts, etc.

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ Download and use NPM (requires installation first) to run
 * API
 Download the source and build in Visual Studio 2015 (requires installation first) (with automatic Nuget Package restore). To run, debug in visual studio
 
+### Semantic UI theming ###
+
+Semantic themes are defined in `/web/semantic`. If you change any parts of the theme, you'll need to re-build `semantic.min.css` by running `gulp build` in `/web/semantic`. The new `semantic.min.css` should be automatically picked up by webpack.
--- web/src/app/AppPresenter.tsx
+++ web/src/app/AppPresenter.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Container, Grid, Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
-import "./AppStyles.scss";
 import BackgroundJobs from "./BackgroundJobs/BackgroundJobsContainer";
 import Home from "./home/HomeContainer";
 import Messaging from "./messages/MessagingContainer";
@@ -10,15 +9,15 @@ import Settings from "./settings/SettingsContainer";
 export class AppPresenter extends React.Component<IAppProps, undefined> {
     public render() {
         return <div>
-                <Nav/>
-                <Container>
-                    <Messaging/>
-                    <br/>
-                    <Home/>
-                    <BackgroundJobs/>
-                    <Settings/>
-                </Container>
-         </div>;
+            <Nav />
+            <Container>
+                <Messaging />
+                <br />
+                <Home />
+                <BackgroundJobs />
+                <Settings />
+            </Container>
+        </div>;
     }
 }
 
