9bc08347991c3e842f9529e3c8ca8e9ed068f046
Rick <rickpowell1311@gmail.com>
2017-03-24 20:07:08 +0000

    Add Services Tests project and consolidate nuget packages

---
+++ api/src/Box9.Leds.Manager.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Box9.Leds.Manager.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Box9.Leds.Manager.Services.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("055b63a8-03da-46d0-bc11-eba6775fed17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
