6ace3efd2892ab2d9808fe0e1f3c4d590cd3a39c
Rick <rickpowell1311@gmail.com>
2017-03-25 13:04:54 +0000

    Allow configurable Pi API port

--- api/src/Box9.Leds.Manager.PiApiClient/PiApiClientFactory.cs
+++ api/src/Box9.Leds.Manager.PiApiClient/PiApiClientFactory.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using Box9.Leds.Manager.DataAccess.Models;
+using Box9.Leds.Manager.Core.Config;
 
 namespace Box9.Leds.Manager.PiApiClient
 {
     public class PiApiClientFactory : IPiApiClientFactory
     {
+        private readonly IConfiguration config;
+
+        public PiApiClientFactory(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         public IPiApiClient ForDevice(Device device)
         {
-            return new PiApiClient(new Uri(string.Format("http://{0}:8001/api", device.IpAddress)));
+            var piApiPort = int.Parse(config.AppSettings["PiApiPort"]);
+
+            return new PiApiClient(new Uri(string.Format("http://{0}:{1}/api", device.IpAddress, piApiPort)));
         }
     }
 }
