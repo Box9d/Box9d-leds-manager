8d224fe62b31f35e9a039912f84407da948c72bf
Rick <rickpowell1311@gmail.com>
2017-03-25 13:24:39 +0000

    Add Pi API host

---
+++ api/src/Box9.Leds.Pi.Api.WebHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Configuration;
+
+namespace Box9.Leds.Pi.Api.WebHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = ConfigurationManager.AppSettings["Host"];
+            using (var app = WebApp.Start<Startup>(baseUrl))
+            {
+                Console.WriteLine("Starting Pi API...");
+                Console.WriteLine("Press any key to stop");
+                Console.ReadKey();
+            }
+        }
+    }
+}
