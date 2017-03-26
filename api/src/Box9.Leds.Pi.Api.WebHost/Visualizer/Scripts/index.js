20013f26449e21bd57de6bee0a757cd32cc383c1
Rick <rickpowell1311@gmail.com>
2017-03-26 18:34:52 +0100

    Update to latest version of Pi API

---
+++ api/src/Box9.Leds.Pi.Api.WebHost/Visualizer/Scripts/index.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    var playback = $.connection.playbackHub;
+    playback.client.broadcastMessage = function (name, message) {
+        if (name.toLowerCase() === 'blackout') {
+            $('#display').empty();
+        }
+
+        if (name.toLowerCase() === 'frame') {
+            var html = '';
+            var pixelsPerRow = Math.ceil(message.length / $('#rowcount').val());
+
+            for (var i = 0; i < $('#rowcount').val(); i++) {
+                var rowDiv = '<div class=\'row\' style=\'width:' + pixelsPerRow * 15 + 'px;\'>';
+                for (var j = 0; j < pixelsPerRow; j++) {
+                    var color = message[i * pixelsPerRow + j];
+                    rowDiv += '<div class=\'pixel\' style=\'background-color:' + color + '\'/>';
+                }
+
+                rowDiv += '</div>';
+                html += rowDiv;
+            }
+
+            $('#display').html(html);
+        }
+    };
+
+    $.connection.hub.start().done(function () {
+    });
+});
+
+$(function () {
+    $('#rowcount').bind('input', function () {
+        if ($(this).val() === "0") {
+            $(this).val("1"); // Do not allow 0 value
+        }
+    });
+});
