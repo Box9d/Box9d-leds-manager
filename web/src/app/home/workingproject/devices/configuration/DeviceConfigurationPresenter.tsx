367ac0be94cc3a293760b16dea9cfeb5cb612df9
Rick Powell <richard.powell@sfwltd.co.uk>
2017-03-28 18:40:59 +0100

    Simplify validator in device configuration

--- web/src/app/home/workingproject/devices/configuration/DeviceConfigurationPresenter.tsx
+++ web/src/app/home/workingproject/devices/configuration/DeviceConfigurationPresenter.tsx
@@ -6,27 +6,21 @@ import { DeviceConfigurationValidator } from "./DeviceConfigurationValidator";
 
 export class DeviceConfigurationsPresenter extends React.Component<IDeviceConfigurationProps, undefined> {
 
-    private validator: DeviceConfigurationValidator;
-
-    constructor(props: IDeviceConfigurationProps) {
-        super(props);
-
-        this.validator = new DeviceConfigurationValidator(this.props);
-    }
-
     public render(): JSX.Element {
 
+        let validator: DeviceConfigurationValidator = new DeviceConfigurationValidator(this.props);
+
         return <div className="device-config-panel">
-                <Form error={!this.validator.validateForm().isValid}>
+                <Form error={!validator.validateForm().isValid}>
                     <Divider horizontal>Pixels</Divider>
                     <Form.Group widths="equal">
-                        <Form.Field error={!this.validator.validateHorizontalPixels().isValid}>
+                        <Form.Field error={!validator.validateHorizontalPixels().isValid}>
                             <Form.Input label="Horizontal pixels" placeholder="Width in pixels" type="number" value={this.props.deviceConfiguration.numberOfHorizontalPixels} onChange={(e) => { this.props.horizontalPixelsOnChange((e as any).target.value); } } />
-                            {!this.validator.validateHorizontalPixels().isValid && <Label basic color="red" pointing>{this.validator.validateHorizontalPixels().errorMessage}</Label>}                        
+                            {!validator.validateHorizontalPixels().isValid && <Label basic color="red" pointing>{validator.validateHorizontalPixels().errorMessage}</Label>}                        
                         </Form.Field>
-                        <Form.Field error={!this.validator.validateVerticalPixels().isValid}>
+                        <Form.Field error={!validator.validateVerticalPixels().isValid}>
                             <Form.Input label="Vertical pixels" placeholder="Height in pixels" type="number" value={this.props.deviceConfiguration.numberOfVerticalPixels} onChange={(e) => { this.props.verticalPixelsOnChange((e as any).target.value); } } />
-                            {!this.validator.validateVerticalPixels().isValid && <Label basic color="red" pointing>{this.validator.validateVerticalPixels().errorMessage}</Label>}
+                            {!validator.validateVerticalPixels().isValid && <Label basic color="red" pointing>{validator.validateVerticalPixels().errorMessage}</Label>}
                         </Form.Field>
                     </Form.Group>
                     <Divider horizontal>Mapping</Divider>
@@ -38,10 +32,6 @@ export class DeviceConfigurationsPresenter extends React.Component<IDeviceConfig
                     <Button disabled={!this.props.isMappingConfigured} color="green">Save</Button>
                 </Form>
             </div>;
-    }
-
-    public componentWillReceiveProps(nextProps: IDeviceConfigurationProps): void {
-        this.validator = new DeviceConfigurationValidator(nextProps);
     }
 }
 
