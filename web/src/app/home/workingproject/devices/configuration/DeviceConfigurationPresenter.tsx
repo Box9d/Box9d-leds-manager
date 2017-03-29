88494ed038d4b9601c06635615dccaea9a7a64d6
Rick Powell <richard.powell@sfwltd.co.uk>
2017-03-29 09:17:35 +0100

    Implement validation of horizontal and vertical pixels

--- web/src/app/home/workingproject/devices/configuration/DeviceConfigurationPresenter.tsx
+++ web/src/app/home/workingproject/devices/configuration/DeviceConfigurationPresenter.tsx
@@ -4,27 +4,33 @@ import * as ApiClient from "../../../../../../../api/build/ApiClient";
 import DeviceConfigurationModal from "./DeviceConfigurationModalContainer";
 import { DeviceConfigurationValidator } from "./DeviceConfigurationValidator";
 
-export class DeviceConfigurationsPresenter extends React.Component<IDeviceConfigurationProps, undefined> {
+export class DeviceConfigurationsPresenter extends React.Component<IDeviceConfigurationProps, IDeviceConfigurationState> {
+
+    constructor(props: IDeviceConfigurationProps) {
+        super(props);
+
+        this.state = {shouldValidate: false};
+    }
 
     public render(): JSX.Element {
 
         let validator: DeviceConfigurationValidator = new DeviceConfigurationValidator(this.props);
 
         return <div className="device-config-panel">
-                <Form error={!validator.validateForm().isValid}>
+                <Form error={this.state.shouldValidate && !validator.validateForm().isValid}>
                     <Divider horizontal>Pixels</Divider>
                     <Form.Group widths="equal">
-                        <Form.Field error={!validator.validateHorizontalPixels().isValid}>
+                        <Form.Field error={this.state.shouldValidate && !validator.validateHorizontalPixels().isValid}>
                             <Form.Input label="Horizontal pixels" placeholder="Width in pixels" type="number" value={this.props.deviceConfiguration.numberOfHorizontalPixels} onChange={(e) => { this.props.horizontalPixelsOnChange((e as any).target.value); } } />
-                            {!validator.validateHorizontalPixels().isValid && <Label basic color="red" pointing>{validator.validateHorizontalPixels().errorMessage}</Label>}                        
+                            {this.state.shouldValidate && !validator.validateHorizontalPixels().isValid && <Label basic color="red" pointing>{this.state.shouldValidate && validator.validateHorizontalPixels().errorMessage}</Label>}                        
                         </Form.Field>
-                        <Form.Field error={!validator.validateVerticalPixels().isValid}>
+                        <Form.Field error={this.state.shouldValidate && !validator.validateVerticalPixels().isValid}>
                             <Form.Input label="Vertical pixels" placeholder="Height in pixels" type="number" value={this.props.deviceConfiguration.numberOfVerticalPixels} onChange={(e) => { this.props.verticalPixelsOnChange((e as any).target.value); } } />
-                            {!validator.validateVerticalPixels().isValid && <Label basic color="red" pointing>{validator.validateVerticalPixels().errorMessage}</Label>}
+                            {this.state.shouldValidate && !validator.validateVerticalPixels().isValid && <Label basic color="red" pointing>{this.state.shouldValidate && validator.validateVerticalPixels().errorMessage}</Label>}
                         </Form.Field>
                     </Form.Group>
                     <Divider horizontal>Mapping</Divider>
-                    <Button color="blue" onClick={(e: any) => { e.preventDefault(); this.props.onModalOpened(); }}>Configure mapping</Button>
+                    <Button color="blue" onClick={this.configureMappingOnClick}>Configure mapping</Button>
                     {this.props.isMappingConfigured && <Label color="green">OK</Label>}
                     {!this.props.isMappingConfigured && <Label color="yellow">Not mapped</Label>}
                     { this.props.modalIsOpen && <DeviceConfigurationModal /> }
@@ -32,6 +38,17 @@ export class DeviceConfigurationsPresenter extends React.Component<IDeviceConfig
                     <Button disabled={!this.props.isMappingConfigured} color="green">Save</Button>
                 </Form>
             </div>;
+    }
+
+    private configureMappingOnClick = (e: any): void => {
+        e.preventDefault();
+
+        let validator: DeviceConfigurationValidator = new DeviceConfigurationValidator(this.props);
+        if (!validator.validateForm().isValid) {
+            this.setState({shouldValidate: true});
+        } else {
+            this.props.onModalOpened();
+        }
     }
 }
 
@@ -43,3 +60,7 @@ export interface IDeviceConfigurationProps {
     horizontalPixelsOnChange?: (value: number) => void;
     verticalPixelsOnChange?: (value: number) => void;
 };
+
+export interface IDeviceConfigurationState {
+    shouldValidate: boolean;
+}
