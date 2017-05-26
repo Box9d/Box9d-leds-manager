d414dbba6169047895661c311a48c51a6b88ce0c
Rick Powell <richard.powell@sfwltd.co.uk>
2017-05-26 11:38:33 +0100

    Add time controller for querying from PIs

---
+++ api/src/Box9.Leds.Manager.Api/Controllers/TimeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Box9.Leds.Manager.Api.Controllers
+{
+    public class TimeController : ApiController
+    {
+        [HttpGet]
+        [ActionName("/")]
+        public GlobalJsonResult<DateTime> Get()
+        {
+            return GlobalJsonResult<DateTime>.Success(System.Net.HttpStatusCode.OK, DateTime.Now);
+        }
+    }
+}
