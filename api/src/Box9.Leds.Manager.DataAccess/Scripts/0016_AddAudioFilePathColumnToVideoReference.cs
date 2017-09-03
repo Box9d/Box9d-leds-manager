429760c9a23b3942ca091b2f96ddf788220c152d
Rick <rickpowell1311@gmail.com>
2017-09-03 19:48:08 +0100

    Re-add Scripts/0016_AddAudioFilePathColumnToVideoReference.cs after merge

---
+++ api/src/Box9.Leds.Manager.DataAccess/Scripts/0016_AddAudioFilePathColumnToVideoReference.cs
@@ -0,0 +1,11 @@
+﻿using InstaSqlite;
+
+namespace Box9.Leds.Manager.DataAccess.Scripts
+{
+    public class _0016_AddAudioFilePathColumnToVideoReference : IScript
+    {
+        public string Sql => "ALTER TABLE VideoReference ADD audiofilepath TEXT";
+
+        public int Id => 16;
+    }
+}
